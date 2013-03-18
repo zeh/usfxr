2ab46263f076980b509ac383f91b9c3a91b1e9a4
Zeh Fernando <zeh@zehfernando.com>
2013-03-18 14:19:24 -0400

    Additional audio effects and proper project commit

--- samples/main/Assets/Scripts/Main.cs
+++ samples/main/Assets/Scripts/Main.cs
@@ -5,6 +5,11 @@ public class Main : MonoBehaviour {
 
 	private bool isADown;
 	private bool isBDown;
+	private bool isCDown;
+
+	private SfxrSynth synthA;
+	private SfxrSynth synthB;
+	private SfxrSynth synthC;
 
 	void Start () {
 		isADown = false;
@@ -24,26 +29,42 @@ public class Main : MonoBehaviour {
 	void Update () {
 		bool newIsADown = Input.GetKey("a");
 		bool newIsBDown = Input.GetKey("b");
+		bool newIsCDown = Input.GetKey("c");
 
 		if (newIsADown && !isADown) {
 			Debug.Log("Key: A");
 
-			SfxrSynth synth = new SfxrSynth();
-			synth.paramss.setSettingsString("0,,0.032,0.4138,0.4365,0.834,,,,,,0.3117,0.6925,,,,,,1,,,,,0.5");
-			synth.cacheSound();
-			synth.play();
+			if (synthA == null) {
+				synthA = new SfxrSynth();
+				synthA.paramss.setSettingsString("0,,0.032,0.4138,0.4365,0.834,,,,,,0.3117,0.6925,,,,,,1,,,,,0.5");
+				synthA.cacheSound();
+			}
+
+			synthA.play();
 		}
 		if (newIsBDown && !isBDown) {
 			Debug.Log("Key: B");
 
-			SfxrSynth synth = new SfxrSynth();
-			synth.paramss.generatePickupCoin();
-			//synth.cacheSound();
-			synth.paramss.setSettingsString("0,,0.032,0.4138,0.4365,0.834,,,,,,0.3117,0.6925,,,,,,1,,,,,0.5");
-			synth.play();
+			if (synthB == null) {
+				synthB = new SfxrSynth();
+				synthB.paramss.setSettingsString("0,,0.032,0.4138,0.4365,0.834,,,,,,0.3117,0.6925,,,,,,1,,,,,0.5");
+			}
+
+			synthB.play();
+		}
+		if (newIsCDown && !isCDown) {
+			Debug.Log("Key: C");
+
+			if (synthC == null) {
+				synthC = new SfxrSynth();
+				synthC.paramss.setSettingsString("2,,0.1702,,0.1689,0.7793,0.0224,-0.4882,,,,,,0.271,0.1608,,,,1,,,,,0.5");
+			}
+
+			synthC.play();
 		}
 
 		isADown = newIsADown;
 		isBDown = newIsBDown;
+		isCDown = newIsCDown;
 	}
 }
