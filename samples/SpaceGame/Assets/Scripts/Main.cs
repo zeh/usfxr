adda8a69b3b2d35772549a9ad4b44a1e8b640202
zeh <zeh@zehfernando.com>
2013-04-21 20:16:25 -0400

    Some gameplay/aesthetical changes

--- samples/SpaceGame/Assets/Scripts/Main.cs
+++ samples/SpaceGame/Assets/Scripts/Main.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Main : MonoBehaviour {
 
 	private const float TIME_ENEMY_SPAWN_INTERVAL_START = 2; // In seconds
+	private const float TIME_ENEMY_SPAWN_INTERVAL_MIN = 0.5f; // In seconds
 
 	private float timeEnemySpawned;
 	private float timeEnemySpawnInterval;
@@ -18,7 +19,8 @@ public class Main : MonoBehaviour {
 	
 	void Update() {
 		if (Time.realtimeSinceStartup > timeEnemySpawned + timeEnemySpawnInterval) {
-			timeEnemySpawnInterval *= 0.98f; // Makes it go faster and faster over time
+			timeEnemySpawnInterval *= 0.97f; // Makes it go faster and faster over time
+			if (timeEnemySpawnInterval < TIME_ENEMY_SPAWN_INTERVAL_MIN) timeEnemySpawnInterval = TIME_ENEMY_SPAWN_INTERVAL_MIN;
 			SpawnEnemy();
 		}
 
